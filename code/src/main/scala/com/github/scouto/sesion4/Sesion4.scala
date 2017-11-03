bc9bb6026a48a6bb6ac35767340dda92ca1bd142
Pablo <padominca@gmail.com>
2017-11-03 13:52:33 +0100

    Sesion 5

--- code/src/main/scala/com/github/scouto/sesion4/Sesion4.scala
+++ code/src/main/scala/com/github/scouto/sesion4/Sesion4.scala
@@ -6,13 +6,20 @@ object Sesion4 extends App{
      go(List(),list)
 
     @annotation.tailrec
-    def go[A](acc: List[A], rest: List[A]): List[A] = {
+    def go(acc: List[Int], rest: List[Int]): List[Int] = {
       rest match {
         case Nil => acc
         case h::t if f(h) => acc:::t //si cumple devuelvo la lista sin el
         case h::t if !f(h) => go(acc :+ h, t) //si no cumple paso el elemento al acumulado
       }
     }
+    /*def go[A](acc: List[A], rest: List[A]): List[A] = {
+      rest match {
+        case Nil => acc
+        case h::t if f(h) => acc:::t //si cumple devuelvo la lista sin el
+        case h::t if !f(h) => go(acc :+ h, t) //si no cumple paso el elemento al acumulado
+      }
+    }*/
   }
 
   def fb(x: Int): BigInt = {
@@ -24,14 +31,36 @@ object Sesion4 extends App{
     }
   }
 
-  //def msort[T](l: List[T], less(T, T) => Boolean):List[T] = {  }
-  def msort[T]( xs: List[T])(implicit less: (T, T) => Boolean): List[T] = {
+  def msort[T](l: List[T], less:(T, T) => Boolean):List[T] = {
+    @annotation.tailrec
+    def accsort (acc: List[T], pending: List[T]): List[T] ={
+      pending match {
+        case h::Nil => acc
+        case h::t if less(h, t.head) && !isSorted(t.tail) => accsort(acc:::List(h):::List(t.head), t.tail)
+        case h::t if less(h, t.head) && isSorted(t.tail) => acc:::pending
+        case h::t if !less(h, t.head) => accsort(acc:::List(t.head):::List(h), t.tail)
+        case _ => acc
+
+      }
+
+    }
+    accsort(List(), l)
 
   }
 
-  //def isSorted[A](as: List[A])(implicit ordered: (A,A) => Boolean): Boolean = {  }
   def isSorted[A](as: List[A])(implicit ordered: (A,A) => Boolean): Boolean = {
 
+    @annotation.tailrec
+    def loop (rest: List[A]): Boolean = {
+      rest match {
+        case Nil => true
+        case h::Nil => true
+        case h1::t if ordered(h1, t.head) => loop (t)
+        case h1::t if !ordered(h1, t.head) => false
+      }
+    }
+    loop (as)
   }
+
 }
 
--- code/src/main/scala/com/github/scouto/sesion5/Lista.scala
+++ code/src/main/scala/com/github/scouto/sesion5/Lista.scala
@@ -15,17 +15,67 @@ object Lista {
     if (as.isEmpty) Vacio
     else Cons(as.head, apply(as.tail: _*))
 
-  def sum(ints: Lista[Int]): Int = ???
+  def sum(ints: Lista[Int]): Int = {
+    ints match {
+      case Vacio => 0
+      case Cons (h, t) => h + sum(t)
+    }
+  }
 
-  def product(ints: Lista[Double]) : Double = ???
+  def product(ints: Lista[Double]) : Double = {
+    ints match {
+      case Vacio => 1.0
+      case Cons (h, t) => h * product(t)
+    }
+  }
 
-  def tail[A](list: Lista[A]): Lista[A] = ???
+  def tail[A](list: Lista[A]): Lista[A] = {
+    list match {
+      case Vacio => Vacio
+      case Cons (h, t) => t
+    }
+  }
 
-  def setHead[A](list: Lista[A], newHead: A): Lista[A] = ???
+  def setHead[A](list: Lista[A], newHead: A): Lista[A] = {
+    list match {
+      case Vacio => Lista(newHead)
+      case Cons (h, t) => Cons(newHead,t)
+    }
+  }
 
-  def drop[A](list: Lista[A],n: Int): Lista[A] = ???
+  def drop[A](list: Lista[A],n: Int): Lista[A] = {
+    @annotation.tailrec
+    def goDrop(rest: Lista[A],n: Int): Lista[A] = {
+      list match {
+        case Vacio => Vacio
+        case Cons (h, t) if (n == 0) => t
+        case Cons (h, t) if (n > 0) => goDrop(t, n-1)
+        case Cons (h, t) if (n < 0) => list
+      }
+    }
 
-  def dropWhile[A](list: Lista[A], f: A => Boolean): Lista[A] = ???
+    list match {
+      case Vacio => Vacio
+      case Cons (h, t) => goDrop(list,n)
+    }
+  }
+
+  def dropWhile[A](list: Lista[A], f: A => Boolean): Lista[A] = {
+    @annotation.tailrec
+    def goDropWhile(rest: Lista[A]): Lista[A] = {
+      list match {
+        case Vacio => Vacio
+        case Cons (h, t) if f(h) => goDropWhile(t)
+        case Cons (h, t) if !f(h)=> rest
+      }
+    }
+
+    list match {
+      case Vacio => Vacio
+      case Cons (h, t) => goDropWhile(list)
+    }
+
+  }
 
   def append[A](l1: Lista[A], l2: Lista[A]): Lista[A] = {
 
@@ -35,8 +85,21 @@ object Lista {
     }
   }
 
+ //recibida una lista, devuelva una Lista con todos los elementos excepto el último
+  def init[A](l: Lista[A]): Lista[A] = {
+    @annotation.tailrec
+    def loop(acc: Lista[A], rest: Lista[A]): Lista[A] = {
+      rest match {
+        case Vacio => acc
+        case Cons(h,t) =>  t match {
+                              case Vacio => acc
+                              case Cons(h,t) => loop(append(acc, Lista(h)),t)
+                            }
+      }
 
-  def init[A](l: Lista[A]): Lista[A] = ???
+    }
+    loop(Lista(), l)
+  }
 
   }
 
